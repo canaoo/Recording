b727d640fbb9e54fc226be3cd2c9fd6c0a92f34f
canaoo <kana-nagamine-20c@stu.kbc.ac.jp>
2022-11-14 22:25:20 +0900

    modigy bashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,21 @@
-<x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    ログインしました!<br><br>
-                    <a href="{{ route('top') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">トップ画面へ移動する</a>
+ <!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Keyboard Recordings</title>
+        <link href="{{ secure_asset('build/assets/custom.css') }}" rel="stylesheet" media="all">
+    </head>
+    <body>
+        <x-app-layout>
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 bg-white border-b border-gray-200">
+                            <p>ログインしました!</p><br>
+                            <a href="{{ route('top') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">トップ画面へ移動する</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</x-app-layout>
+        </x-app-layout>
+    </body>
+</html>
