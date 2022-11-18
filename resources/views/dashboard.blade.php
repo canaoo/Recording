2478ed5f051d5b05c3d42400afdc5a1269e3e13f
canaoo <kana-nagamine-20c@stu.kbc.ac.jp>
2022-11-18 22:20:58 +0900

    modify dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,11 @@
         <x-app-layout>
             <div class="py-12">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg center">
                         <div class="p-6 bg-white border-b border-gray-200">
-                            <p>ログインしました!</p><br>
-                            <a href="{{ route('top') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">トップ画面へ移動する</a>
+                            <h1 style="font-size: 30px;">Keyboard Recordingsへようこそ</h1>
+                            <br><br><p>ログインしました !</p><br>
+                            <a href="{{ route('top') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">トップ画面へ</a><br><br>
                         </div>
                     </div>
                 </div>
