2517312093657eb8a03f5cc0048e31cd33bd530f
canaoo <kana-nagamine-20c@stu.kbc.ac.jp>
2022-12-22 20:19:35 +0900

    modify forgot-password.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,7 +40,6 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('次回から自動的にログインする') }}</span>
                 </label>
             </div>
-
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
